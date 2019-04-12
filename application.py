e666916dad94f08cf10bd20f09c4b62e7f867028
rvl24 <rvl24@users.noreply.github.com>
2019-04-12 03:47:29 +0000

    20190412T034722Z

--- application.py
+++ application.py
@@ -32,9 +32,9 @@ def get_form():
 @app.route("/form", methods=["POST"])
 def post_form():
     with open("survey.csv", "a", newline="") as csv_file:
-        well_info=[request.form.get("well_id"), \
-            request.form.get("road_box"), request.form.get("depth_to_water"), \
-            request.form.get("depth_to_bottom"), request.form.get("well_condition")]
+        well_info = [request.form.get("well_id"),
+                     request.form.get("road_box"), request.form.get("depth_to_water"),
+                     request.form.get("depth_to_bottom"), request.form.get("well_condition")]
         writer = csv.writer(csv_file)
         writer.writerow(well_info)
     return get_sheet()
@@ -45,7 +45,7 @@ def get_sheet():
     headers = ["Well ID", "Road Box", "Depth to Water", "Depth to Bottom", "Well Condition"]
     with open("survey.csv", "r", newline="") as csv_file:
         reader = csv.reader(csv_file)
-        rows=[]
+        rows = []
         for line in reader: 
             rows.append(line)
     return render_template("sheet.html", headers=headers, rows=rows)
